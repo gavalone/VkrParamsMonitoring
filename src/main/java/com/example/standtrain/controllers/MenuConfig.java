e3c98de1b27cebcce2a8c5dc9c8d986a048b1f92
ermolaeva <ermolaeva@lcard.ru>
2026-04-10 18:26:50 +0300

    fixed bugs and added comments

--- src/main/java/com/example/standtrain/controllers/MenuConfig.java
+++ src/main/java/com/example/standtrain/controllers/MenuConfig.java
@@ -1,7 +1,8 @@
 package com.example.standtrain.controllers;
 
 import com.example.standtrain.services.*;
-import javafx.application.*;
+import com.example.standtrain.util.*;
+import javafx.application.Platform;
 import javafx.fxml.*;
 import javafx.scene.control.*;
 
@@ -10,6 +11,10 @@ import static com.example.standtrain.util.Globals.threadE16running;
 import static com.example.standtrain.util.Globals.adcThread;
 import static com.example.standtrain.util.Globals.handleE16initialized;
 import static com.example.standtrain.util.Globals.logs;
+import static com.example.standtrain.util.Globals.handleLTAinitialized;
+import static com.example.standtrain.util.Globals.ltaThread;
+import static com.example.standtrain.util.Globals.threadLTArunning;
+
 
 /**
  Класс контроллера основной страницы с запуском потоков и инициализацией устройств
@@ -36,70 +41,118 @@ public class MenuConfig {
         }
     }
 
+    /* Инициализация модуля Е16 */
     public void e16initialize(){
-        if (!handleE16initialized) {
-            try {
-                handleE16 = Initialization.createHandle();
+        try {
+            if (!handleE16initialized) {
+                handleE16 = InitializationE16.createHandle();
 
-                status = Initialization.openE16(handleE16);
+                status = InitializationE16.openE16(handleE16);
                 addTextToScroll("openE16: " + status);
 
-                status = Initialization.setE16ChannelsCount(handleE16, 4);
+                status = InitializationE16.setE16ChannelsCount(handleE16, 4);
                 addTextToScroll("setE16ChannelsCount: " + status);
 
-                status = Initialization.iniE16Channels(handleE16, 4);
+                status = InitializationE16.iniE16Channels(handleE16, 4);
                 addTextToScroll("iniE16Channels: " + status);
 
-                status = Initialization.X502_SetAdcFreq(handleE16);
+                status = InitializationE16.X502_SetAdcFreq(handleE16);
                 addTextToScroll("X502_SetAdcFreq: " + status);
 
-                status = Initialization.put16Data(handleE16);
+                status = InitializationE16.put16Data(handleE16);
                 addTextToScroll("put16Data: " + status);
 
                 iniVoltageAndRelay();
                 if (status==0){
                     handleE16initialized = true;
                 }
+            }
+        }
+        catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
 
-            } catch (Exception e) {
+    /* Инициализация устройства LTA */
+    public void LTAInititialize(){
+        if (!handleLTAinitialized){
+            try {
+                status = InitializationLTA.openLTAByIp();
+                addTextToScroll("openLTA: " + status);
+
+                status = InitializationLTA.initLTA();
+                addTextToScroll("iniLTA: " + status);
+
+                status = InitializationLTA.enableChannels();
+                addTextToScroll("ini channels: " + status);
+
+                if (status == 0){
+                    handleLTAinitialized = true;
+                }
+            }
+            catch (Exception e) {
                 System.out.println(e.getMessage());
             }
         }
     }
 
-/* Подача напряжения на рельсы и включение реле */
+    /* Запуск потока обработки данных с LTA */
+    public void launchLTAThread(){
+        if (handleLTAinitialized) {
+            if (ltaThread != null && ltaThread.isAlive()) return;
+            addTextToScroll("LTA thread started");
+            threadLTArunning = true;
+            ltaThread = DataOutputLTA.startAsynchroAcquisitionLTA();
+        }
+    }
+
+    /* Остановка потока обработки с LTA */
+    public void closeLTAThread(){
+        if (handleLTAinitialized) {
+            if (ltaThread == null) return;
+            threadLTArunning = false;
+            try {
+                ltaThread.join(3000);
+                addTextToScroll("LTA thread stopped");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /* Подача напряжения на рельсы и включение реле */
     public void iniVoltageAndRelay(){
-        status = DataInput.putV(2.5, handleE16);
+        status = DataInputE16.putV(2.5, handleE16);
         addTextToScroll("putV: " + status);
-        status = Initialization.iniRelay(handleE16);
+        status = InitializationE16.iniRelay(handleE16);
         addTextToScroll("iniRelay: " + status);
     }
 
+    /* Запуск потока обработки данных с E16 */
     public void launchADCThread() {
         if (adcThread != null && adcThread.isAlive()) return;
-
-        status = DataOutput.streamsEnable(handleE16);
+        status = DataOutputE16.streamsEnable(handleE16);
         addTextToScroll("streamsEnable: " + status);
-        status = DataOutput.streamsStart(handleE16);
+        status = DataOutputE16.streamsStart(handleE16);
         addTextToScroll("streamsStart: " + status);
-
         threadE16running = true;
-        adcThread = DataOutput.startSynchroAcquisition(handleE16, 4);
+        adcThread = DataOutputE16.startSynchroAcquisition(handleE16, Consts.chNum);
     }
 
+    /* Остановка потока обработки с Е16*/
     public void stopADCThread() {
         if (adcThread == null) return;
 
         threadE16running = false;
         try {
-            adcThread.join();
+            adcThread.join(3000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        status = DataOutput.streamsStop(handleE16);
+        status = DataOutputE16.streamsStop(handleE16);
         addTextToScroll("streamsStop: " + status);
-        status = DataOutput.streamsDisable(handleE16);
+        status = DataOutputE16.streamsDisable(handleE16);
         addTextToScroll("streamsDisable: " + status);
     }
 }
