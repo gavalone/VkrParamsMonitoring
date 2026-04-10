b1e10a3387339adfbbc797de337436c4e88e3fc3
ermolaeva <ermolaeva@lcard.ru>
2026-04-10 18:39:33 +0300

    fixed

---
+++ src/main/java/com/example/standtrain/interfaces/X502Api.java
@@ -0,0 +1,28 @@
+package com.example.standtrain.interfaces;
+
+import com.sun.jna.*;
+import com.sun.jna.ptr.*;
+
+/**
+ * Интерфейс для объяления методов для устройства E16 (логика работы)
+ */
+public interface X502Api extends Library {
+    X502Api INSTANCE = Native.load("x502api.dll", X502Api.class);
+    Pointer X502_Create();
+    int X502_Close(Pointer hdl);
+    int X502_Free(Pointer hdl);
+    int X502_SetLChannelCount(Pointer hnd, int lch_cnt);
+    int X502_SetLChannel(Pointer hnd, int lch, int phy_ch, int mode, int range, int avg);
+    int X502_Configure(Pointer hnd, int flags);
+    int X502_SetDigInPullup(Pointer hnd, int pullups);    //подаем на реле +-15в
+    int X502_AsyncOutDac(Pointer hnd, int ch, double data, int flags); //на канал цап
+    int X502_AsyncOutDig(Pointer hnd, int val, int mask); //цифровые линии
+    int X502_StreamsEnable(Pointer hnd, int streams);
+    int X502_StreamsDisable(Pointer hnd, int streams);
+    int X502_StreamsStart(Pointer hnd);
+    int X502_StreamsStop(Pointer hnd);
+    int X502_Recv(Pointer hnd, Pointer buf, int size, int tout);
+    int X502_ProcessData(Pointer hnd, Pointer src, int size, int flags, Pointer adc_data, IntByReference adc_data_size, Pointer din_data, IntByReference  din_data_size);
+    int X502_GetNextExpectedLchNum(Pointer hnd, IntByReference first_lch);
+    int X502_SetAdcFreq(Pointer hnd, DoubleByReference f_acq, DoubleByReference f_frame);
+}
